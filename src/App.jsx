ee7b09eaed04d085c73bb8185b10fbb18c97cbe1
komilM <komiljonmeliyev315@gmail.com>
2026-01-19 11:03:09 +0500

    nav boshlandi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Nav from './nav/Nav'
+
+const App = () => {
+  return (
+    <div>
+      <Nav></Nav>
+    </div>
+  )
+}
+
+export default App
