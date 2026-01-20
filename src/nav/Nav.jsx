337294a2f87f8d76a4a41ec1fc6b0e9c444905c5
komilM <komiljonmeliyev315@gmail.com>
2026-01-20 11:46:02 +0500

    nav-menu tugadi va nav-menu-2 boshlandi

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Nav.css"
+import searchicon from "../assets/Component 2.png"
+import selected from "../assets/Wishlist.png"
+import savat from "../assets/Cart1 with buy.png"
 
 const Nav = () => {
     return (
@@ -11,7 +14,12 @@ const Nav = () => {
                 <p>About</p>
                 <p>Sign Up</p>
             </div>
-            <input type="text" placeholder='What are you looking for?' />
+            <input type="text" placeholder='What are you looking for ?' />
+            <img style={{ width: "20px", height: "20px", position: "relative", marginLeft: "-230px", marginTop: "9px" }} src={searchicon} alt="" />
+            <div className="nav-menu-2">
+                <img src={selected} alt="" />
+                <img src={savat} alt="" />
+            </div>
         </div>
     )
 }
