27fea3ec7ed53a9a66db6c0c9c8a1eddb8346f86
komilM <komiljonmeliyev315@gmail.com>
2026-01-20 19:42:01 +0500

    Nav tugadi header boshlandi

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -6,20 +6,23 @@ import savat from "../assets/Cart1 with buy.png"
 
 const Nav = () => {
     return (
-        <div className='body'>
-            <h1>Exclusive</h1>
-            <div className="nav-menu">
-                <p>Home</p>
-                <p>Contact</p>
-                <p>About</p>
-                <p>Sign Up</p>
+        <div className=''>
+            <div className="body">
+                <h1>Exclusive</h1>
+                <div className="nav-menu">
+                    <p>Home</p>
+                    <p>Contact</p>
+                    <p>About</p>
+                    <p>Sign Up</p>
+                </div>
+                <input type="text" placeholder='What are you looking for ?' />
+                <img style={{ width: "20px", height: "20px", position: "relative", marginLeft: "-230px", marginTop: "9px" }} src={searchicon} alt="" />
+                <div className="nav-menu-2">
+                    <img src={selected} alt="" />
+                    <img src={savat} alt="" />
+                </div>
             </div>
-            <input type="text" placeholder='What are you looking for ?' />
-            <img style={{ width: "20px", height: "20px", position: "relative", marginLeft: "-230px", marginTop: "9px" }} src={searchicon} alt="" />
-            <div className="nav-menu-2">
-                <img src={selected} alt="" />
-                <img src={savat} alt="" />
-            </div>
+            <hr />
         </div>
     )
 }
